fbdf532af7ef018efff44792b1ed1e9f68efd5d2
Cubicroot <24812442+CubicrootXYZ@users.noreply.github.com>
2020-11-03 19:31:56 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-import imaplib, datetime, random, email, os, configparser
+# Great thanks to the sources I gathered some code from:
+# - http://radtek.ca/blog/delete-old-email-messages-programatically-using-python-imaplib/
+# - https://stackoverflow.com/questions/6225763/downloading-multiple-attachments-using-imaplib
+# - https://stackoverflow.com/questions/43857002/script-in-python-to-download-email-attachments
+
+import imaplib, datetime, random, email, os, configparser, time
 from webdav3.client import Client
 from os import listdir
 from os.path import isfile, join
@@ -20,15 +25,34 @@ class Mailer:
             password (string): imap user password
         """
         try:
-            self.m = imaplib.IMAP4_SSL(server)
+            self.server = server
             self.user = user
             self.password=password
-            self.m.login(self.user, self.password)
         except Exception as e:
             print(e)
             print("Could not connect to mail server")
             self.m=False
             raise Exception("Could not connect to mail server")
+
+    def start(self):
+        """Starts the connection to the mailserver
+
+        Raises:
+            Exception: Could not connect to mail server
+        """
+        try:
+            self.m = imaplib.IMAP4_SSL(self.server)
+            self.m.login(self.user, self.password)
+        except Exception as e:
+            print(e)
+            print("Could not connect to mail server")
+            raise Exception("Could not connect to mail server")
+    
+    def stop(self):
+        """Stops the connection to the mailserver
+        """
+        self.m.close()
+        self.m.logout()
 
     def downloaAttachmentsInEmail(self, emailid):
         """Downloads the attachments to a local disk
@@ -46,21 +70,47 @@ class Mailer:
                 open("attachments" + '/' + part.get_filename(), 'wb').write(part.get_payload(decode=True))
 
     def run(self):
+        """Runner for the attachment download, call this function
+
+        Returns:
+            False: if there is an exception
+        """
         try:
-            
-            self.m.select()
-            self.m.select("Inbox")
-            typ, msgs = self.m.search(None, 'UnSeen')
-        except Exception as e:
-            print(e)
-            print("Could not connect to mail server")
-            raise Exception("Could not connect to mail server")
+            self.start()
+        except:
             return False
+        self.m.select()
+        self.m.select("Inbox")
+        typ, msgs = self.m.search(None, 'UnSeen')
         msgs = msgs[0].split()
         for emailid in msgs:
             response, data = self.m.store(emailid, '+FLAGS','\\Seen')
             self.downloaAttachmentsInEmail(emailid)
-        #self.m.logout()
+        self.stop()
+
+    def move_to_trash_before_date(self,days_before):
+        """Deletes old mails in the inbox
+
+        Args:
+            days_before (int): mails since this day before today will be deleted
+
+        Returns:
+            False: if there is an exception
+        """
+        try:
+            self.start()
+        except:
+            return False
+        self.m.select()
+        self.m.select("Inbox")
+        before_date = (datetime.date.today() - datetime.timedelta(days_before)).strftime("%d-%b-%Y")  # date string, 04-Jan-2013
+        typ, data = self.m.search(None, '(SINCE {0})'.format(before_date))  # search pointer for msgs before before_date
+        msgs = data[0].split()
+        for emailid in msgs:
+            response, data = self.m.store(emailid, '+FLAGS','\\Deleted')
+        self.m.expunge()
+        self.stop()
+
 
 class Dav:
     """A class to handle webdav request for uploading the mail attachments
@@ -82,6 +132,14 @@ class Dav:
         self.client=Client(self.options)
 
     def uploadAll(self, dir):
+        """Uploads all attachments to the cloud
+
+        Args:
+            dir (string): existing directory to save the files to
+
+        Returns:
+            False: if something went wrong
+        """
         files = [f for f in listdir("attachments") if isfile(join("attachments", f))]
         year = datetime.datetime.now().strftime("%Y")
         date = datetime.datetime.now().strftime("%m-%d")
@@ -111,15 +169,17 @@ d = Dav(config['dav']['host'], config['dav']['user'], config['dav']['password'])
 last = datetime.datetime.now()-datetime.timedelta(days=1)
 
 while True:
-    if datetime.datetime.now()-last > datetime.timedelta(minutes=1):
+    if datetime.datetime.now()-last > datetime.timedelta(minutes=2):
         print("run")
         last = datetime.datetime.now()
         try:
             ma.run()
             d.uploadAll(config['dav']['save_path'])
+            ma.move_to_trash_before_date(0)
         except Exception as e:
             print(e)
             ma = Mailer(config['email']['imap_host'], config['email']['imap_user'], config['email']['imap_password'])
             d = Dav(config['dav']['host'], config['dav']['user'], config['dav']['password'])
+        time.sleep(120)
 
 
